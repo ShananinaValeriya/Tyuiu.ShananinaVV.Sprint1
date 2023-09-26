bc36dd4e043c08c8ab632ffab90c940dc6e1c378
Tyuiu.ShananinaVV@outlook.com <valeriya.v.2508@gmail.com>
2023-09-26 17:56:41 +0500

    Task5

---
+++ Tyuiu.ShananinaVV.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShananinaVV.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.ShananinaVV.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int k = 89;
+            int wait = 5; 
+            int result = ds.Calculate(k);
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.ShananinaVV.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShananinaVV.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.ShananinaVV.Sprint1.Task5.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Шананина В. В. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразования типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнила: Шананина Валерия Викторовна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+            Console.WriteLine("* Пусть k – целое от 1 до 365. Присвоить целой переменной n значение      *");
+            Console.WriteLine("* 1, 2, ..., 7 в зависимости от того, на какой день недели                *");
+            Console.WriteLine("* (понедельник, вторник, ..., воскресенье) приходится k - й день          *");
+            Console.WriteLine("* невисокосного года, в котором 1 января – понедельник.                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int k;
+
+            Console.WriteLine("Введите значение k: ");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Это {ds.Calculate(k)} день недели");
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
